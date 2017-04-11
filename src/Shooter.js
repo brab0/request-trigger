0d6e3e915e0691071fa8b0c2fc9bdd913a40c28b
Saul Luz <saul.luz@nextip.com.br>
2017-04-11 13:15:50 -0300

    Suporte endpoint ligação

--- src/Shooter.js
+++ src/Shooter.js
@@ -1,17 +1,22 @@
 const http = require('http');
-const querystring = require('querystring');
 
 module.exports.fire = (postData, options, cb) => {
-   var req = http.request(options, (res) => {
-      res.setEncoding('utf8');
-   });
 
+   var req = http.request(options, (res) => {
+        res.setEncoding('utf8');
+
+        res.on('data', (e) => {
+            console.log(e.toString())
+        })
+   });
+   
    req.on('error', (e) => {
       console.log(e);
       console.log(`problem with request: ${e.message}`);
    });
 
-   req.write(querystring.stringify(postData));
+   req.write(JSON.stringify(postData));
    req.end();
    cb();
+
 }
